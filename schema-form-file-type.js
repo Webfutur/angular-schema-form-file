c1065724b90aae7789e13f87325ce0d2d117564c
gpedelagrabe <gpedelagrabe@gmail.com>
2015-03-02 12:24:41 +0100

    Handle Size and Extension errors

--- schema-form-file-type.js
+++ schema-form-file-type.js
@@ -14,17 +14,23 @@ angular.module('schemaForm').config(
             'directives/decorators/bootstrap/file.html'
         );
 
-        var schemaFormatfile = function(name, schema, options) {
+        var schemaFormatFileType = function(name, schema, options) {
             if (schema.type === 'string' && schema.format === 'file' ) {
                 var f = schemaFormProvider.stdFormObj(name, schema, options);
                 f.key = options.path;
                 f.type = 'file';
+                if (schema.allowed_extensions !== undefined ) {
+                    f.allowed_extensions = schema.allowed_extensions;
+                }
+                if (schema.max_size !== undefined ) {
+                    f.max_size = schema.max_size;
+                }
                 options.lookup[sfPathProvider.stringify(options.path)] = f;
                 return f;
             }
         };
 
-        schemaFormProvider.defaults.string.unshift(schemaFormatfile);
+        schemaFormProvider.defaults.string.unshift(schemaFormatFileType);
 
     }
 ]);
@@ -39,23 +45,59 @@ ngSchemaFormFileType.directive('ngSchemaFile', function($upload, $timeout) {
             
             scope.fileReaderSupported = window.FileReader != null && (window.FileAPI == null || FileAPI.html5 != false);
             
+            
+            scope.contains = function(array, obj) {
+                for (var i = 0; i < array.length; i++) {
+                    if (array[i] === obj) {
+                        return true;
+                    }
+                }
+                return false;
+            };            
+            
+            scope.validateExtension = function(file) {
+                var extension = file.name.split('.').pop();
+                if(scope.form.allowed_extensions !== undefined && !scope.contains(scope.form.allowed_extensions, extension)) {
+                    file.invalidExtension = true;
+                    return false;
+                }
+                file.invalidExtension = false;
+                return true;                
+            };
+            
+            scope.validateMaxSize = function(file) {
+                if(scope.form.max_size !== undefined && file.size > scope.form.max_size) {
+                    file.invalidSize = true;
+                    return false;                    
+                }
+                file.invalidSize = false;
+                return true;
+            };
+            
             scope.upload = function (files) {
+               
+                // scope.displayThumbs();
                 
-                scope.displayThumbs();
-                
-                ngModel.$setViewValue('');
-                
+                ngModel.$setViewValue('');                
                 
                 if (files && files.length) {
+                    
                     for (var i = 0; i < scope.files.length; i++) {                
-                        var file = scope.files[i];
+                        var file = scope.files[i];              
+
+                        if(!scope.validateExtension(file) || !scope.validateMaxSize(file)) {
+                            continue;
+                        }
+                        
+                        scope.generateThumb(file); 
+
                         $upload.upload({
                             url: 'endpoint-upload.php',
                             fields: {'username': scope.username},
                             file: file
                         }).progress(function (evt) {
                             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                            console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+                            // console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
                             evt.config.file.filesProgress = progressPercentage;
                         }).success(function (data, status, headers, config) {    
                             
@@ -73,6 +115,8 @@ ngSchemaFormFileType.directive('ngSchemaFile', function($upload, $timeout) {
                             
                         });
                     }
+
+                    
                 }
             };
 
